4d273b2a1e9018681eb49fd321e71c3807b567ee
Henrique Caridade <henrique.ps.caridade@gmail.com>
2025-12-12 03:27:24 +0000

    Fixed Backend Deployment

--- docker-build-images.sh
+++ docker-build-images.sh
@@ -1,4 +1,4 @@
+docker build -t europe-west2-docker.pkg.dev/madeinportugal-store-ds2025/app-repo/backend:latest ./backend --build-arg DB_ENV=production
+docker push europe-west2-docker.pkg.dev/madeinportugal-store-ds2025/app-repo/backend:latest
 docker build -t europe-west2-docker.pkg.dev/madeinportugal-store-ds2025/app-repo/frontend:latest ./MIPS-frontend
 docker push europe-west2-docker.pkg.dev/madeinportugal-store-ds2025/app-repo/frontend:latest
-docker build -t europe-west2-docker.pkg.dev/madeinportugal-store-ds2025/app-repo/backend:latest ./backend
-docker push europe-west2-docker.pkg.dev/madeinportugal-store-ds2025/app-repo/backend:latest
